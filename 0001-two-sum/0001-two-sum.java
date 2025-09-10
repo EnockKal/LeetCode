c8a0b913bf5c558651737cf8678892c3326a6b13
Enock <109170710+EnockKal@users.noreply.github.com>
2025-09-10 04:04:37 -0400

    Time: 2 ms (98.89%), Space: 44.9 MB (80.06%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,28 @@
 class Solution {
-    // public int[] twoSum(int[] nums, int target) {
-    //     for(int i = 0; i < nums.length; i++){
-    //         for(int j = i + 1; j < nums.length; j++){
-    //             if(nums[i] + nums[j] == target){
-    //                 return new int [] {i, j};
-    //             }
-    //         }
-    //     }
-    //     return new int[]{};
-    // }
-
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> myMap = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++){
-            myMap.put(nums[i], i);
+        // Best way (using HashMap) O(n)
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int compliment = target - nums[i];
+            
+            if(map.containsKey(compliment)){
+                return new int[] {map.get(compliment), i};
+            }
+            map.put(nums[i], i);
         }
 
-        for (int i = 0; i < nums.length; i++){
-            int complement = target - nums[i];
-            if (myMap.containsKey(complement) && myMap.get(complement) != i){
-                return new int[]{i, myMap.get(complement)};
-            }
-        }
         return new int[]{};
+
+        // USING FOR LOOP ONLY O(n²)
+
+        // for(int i = 0; i < nums.length; i++){
+        //     for(int j = i + 1; j < nums.length; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        // }
+        // return new int[]{};
     }
 }
